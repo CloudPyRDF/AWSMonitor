dad4ed8211100463caba80ea05711ac208e36c3a
Piotr Tekielak <ptekielak@gmail.com>
2021-06-10 14:36:11 +0200

    Add changes listeners

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,10 @@ import { DocumentRegistry } from '@jupyterlab/docregistry';
 
 import { ICellModel, isCodeCellModel } from "@jupyterlab/cells";
 
+import { IOutputAreaModel } from "@jupyterlab/outputarea";
+
+import { IObservableUndoableList, IObservableList, IObservableString } from "@jupyterlab/observables";
+
 import { NotebookActions, NotebookPanel, INotebookModel } from '@jupyterlab/notebook';
 
 import { IDisposable } from '@lumino/disposable';
@@ -35,33 +39,56 @@ export class ButtonExtension implements DocumentRegistry.IWidgetExtension<Notebo
 	}
 
     async showAWSMonitor(panel: NotebookPanel): Promise<any> {
-        const notebook = panel.content;
-            const newCell = notebook.model?.contentFactory.createCodeCell({});
+
+		const notebook = panel.content;
+        const newCell = notebook.model?.contentFactory.createCodeCell({});
 
 
-            let oldIndex = notebook.activeCellIndex;
-    
-            notebook.model.cells.insert(0, newCell);
-            notebook.activeCellIndex = 0;
-    
-    
-            //notebook.activeCell.hide();
-    
-            const cell: ICellModel = notebook.model.cells.get(0);
-            if (!isCodeCellModel(cell)) {
-            throw new Error("cell is not a code cell.");
-            }
-    
-            cell.value.text = "from IPython.core.display import display, HTML\ndisplay(HTML('<div style=\"display: flex; justify-content: center; align-items: center\"><div style=\"height: 200px; width: 500px;\"><div style=\"height: 35px; width: 495px; background-color: #ffb029; display: flex; flex-direction: row; justify-content: left; align-items: center; padding-left: 5px\"><text style=\"font-weight: 900; margin-right: 30px\">AWSMonitor</text><text style=\"font-weight: 900\">Partitions: 64</text></div><div style=\"height: 35px; width: 440px; background-color: #d6d6d6; display: flex; flex-direction: row; justify-content: space-between; align-items: center;padding-left: 30px; padding-right: 30px\"><text style=\"font-weight: 900;\">Status</text><text style=\"font-weight: 900\">Progress</text><text style=\"font-weight: 900\">Duration</text></div><div style=\"height: 35px; width: 440px; display: flex; flex-direction: row; justify-content: space-between; align-items: center; margin-top: 10px; padding-left: 30px; padding-right: 30px\"><text style=\"font-weight: 900;\">Created</text><div style=\"height: 25px; width: 250px; border-style: solid; background-color: #d6d6d6\"><div style=\"height: 25px; width: 200px; background-color: #80d2ff\"></div></div><text style=\"font-weight: 900;\">6000 ms</text>	</div><div style=\"height: 5px; width: 500px; margin-top: 10px; background-color: #d6d6d6\"></div><div style=\"height: 35px; width: 440px; display: flex; flex-direction: row; justify-content: space-between; align-items: center; margin-top: 10px; padding-left: 30px; padding-right: 30px\"><text style=\"font-weight: 900;\">Finished</text><div style=\"height: 25px; width: 250px; border-style: solid; background-color: #d6d6d6\"><div style=\"height: 25px; width: 100px; background-color: #5bfc60\"></div></div><text style=\"font-weight: 900;\">2000 ms</text>	</div></div></div>'))";
-    
-            await NotebookActions.run(notebook, panel.sessionContext);
-            
-            //console.log(cell.outputs.get(0));
-    
-            //notebook.model.cells.remove(0);
-            notebook.activeCellIndex = oldIndex;
+        let oldIndex = notebook.activeCellIndex;
+
+        notebook.model.cells.insert(0, newCell);
+        notebook.activeCellIndex = 0;
+
+
+        //notebook.activeCell.hide();
+
+        const cell: ICellModel = notebook.model.cells.get(0);
+        if (!isCodeCellModel(cell)) {
+        throw new Error("cell is not a code cell.");
+        }
+
+        cell.value.text = "from IPython.core.display import display, HTML\ndisplay(HTML('<div style=\"display: flex; justify-content: center; align-items: center\"><div style=\"height: 200px; width: 500px;\"><div style=\"height: 35px; width: 495px; background-color: #ffb029; display: flex; flex-direction: row; justify-content: left; align-items: center; padding-left: 5px\"><text style=\"font-weight: 900; margin-right: 30px\">AWSMonitor</text><text style=\"font-weight: 900\">Partitions: 64</text></div><div style=\"height: 35px; width: 440px; background-color: #d6d6d6; display: flex; flex-direction: row; justify-content: space-between; align-items: center;padding-left: 30px; padding-right: 30px\"><text style=\"font-weight: 900;\">Status</text><text style=\"font-weight: 900\">Progress</text><text style=\"font-weight: 900\">Duration</text></div><div style=\"height: 35px; width: 440px; display: flex; flex-direction: row; justify-content: space-between; align-items: center; margin-top: 10px; padding-left: 30px; padding-right: 30px\"><text style=\"font-weight: 900;\">Created</text><div style=\"height: 25px; width: 250px; border-style: solid; background-color: #d6d6d6\"><div style=\"height: 25px; width: 200px; background-color: #80d2ff\"></div></div><text style=\"font-weight: 900;\">6000 ms</text>	</div><div style=\"height: 5px; width: 500px; margin-top: 10px; background-color: #d6d6d6\"></div><div style=\"height: 35px; width: 440px; display: flex; flex-direction: row; justify-content: space-between; align-items: center; margin-top: 10px; padding-left: 30px; padding-right: 30px\"><text style=\"font-weight: 900;\">Finished</text><div style=\"height: 25px; width: 250px; border-style: solid; background-color: #d6d6d6\"><div style=\"height: 25px; width: 100px; background-color: #5bfc60\"></div></div><text style=\"font-weight: 900;\">2000 ms</text>	</div></div></div>'))";
+
+        await NotebookActions.run(notebook, panel.sessionContext);
+        
+        //console.log(cell.outputs.get(0));
+
+		//notebook.model.cells.remove(0);
+		notebook.activeCellIndex = oldIndex;
+
+		notebook.model.cells.changed.connect(this.allCellsSlot, "");
+
+		const cell2: ICellModel = notebook.model.cells.get(1);
+		if (!isCodeCellModel(cell2)) {
+		throw new Error("cell is not a code cell.");
+		}
+
+		cell2.outputs.changed.connect(this.outputSlot, "");
+		cell2.value.changed.connect(this.inputSlot, "");
 		
     }
+
+	inputSlot(sender: IObservableString, args: IObservableString.IChangedArgs) {
+		console.log(args);
+	}
+
+    outputSlot(sender: IOutputAreaModel, args: IOutputAreaModel.ChangedArgs) {
+      console.log(args);
+    }
+
+	allCellsSlot(list: IObservableUndoableList<ICellModel>, args: IObservableList.IChangedArgs<ICellModel>) {
+		console.log(args);
+	  }
 
 }
 
