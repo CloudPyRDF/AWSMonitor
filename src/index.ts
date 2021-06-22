7eb0f35e6245573513911e677f986018077bf3bc
Piotr Tekielak <ptekielak@gmail.com>
2021-06-22 11:41:53 +0200

    Fix some things and add time measurement

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,11 @@ import {
 
 import { DocumentRegistry } from '@jupyterlab/docregistry';
 
-import { ICellModel, isCodeCellModel, ICodeCellModel } from "@jupyterlab/cells";
+import { isCodeCellModel, ICodeCellModel } from "@jupyterlab/cells";
 
 import { IOutputAreaModel } from "@jupyterlab/outputarea";
 
-import { IObservableUndoableList, IObservableList, IObservableString } from "@jupyterlab/observables";
-
-import { NotebookActions, NotebookPanel, INotebookModel } from '@jupyterlab/notebook';
+import { NotebookPanel, INotebookModel } from '@jupyterlab/notebook';
 
 import { IDisposable } from '@lumino/disposable';
 
@@ -31,6 +29,8 @@ export class ButtonExtension implements DocumentRegistry.IWidgetExtension<Notebo
 
 	selectedCell: ICodeCellModel;
 
+	selectedCellHTML: Element;
+
 	lastOutput: string;
 
     monitorHTLM: HTMLDivElement;
@@ -40,6 +40,8 @@ export class ButtonExtension implements DocumentRegistry.IWidgetExtension<Notebo
 	created: number;
 
 	finished: number;
+
+	startTime: number;
 
 	createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
 		// Create the toolbar button
@@ -99,47 +101,31 @@ export class ButtonExtension implements DocumentRegistry.IWidgetExtension<Notebo
 	}
 
 	searchForCellWithAnnotation(panel: NotebookPanel): void {
-        if(this.monitorHTLM != null) {
-            const notebook = panel.content;
-            var cells = notebook.model.cells;
-            for (let i = 0; i < cells.length; i++) {
-                const cell = cells.get(i);
-                if(cell.value.text.includes("#@monitor")) {
-                    if (!isCodeCellModel(cell)) {
-                        throw new Error("cell is not a code cell.");
-                    }
-                    this.selectedCell = cell;
-                    this.addListener();
-                    this.setInfo("Cell found");
-                    this.insertAWSMonitor();
-                    break;
-                }
-            }
-            if(!this.selectedCell) {
-                this.setInfo("No cell with annotation found");
-            }
-        }
+		let cellsFromDomModel = document.getElementsByClassName('lm-Widget p-Widget lm-Panel p-Panel jp-Cell-inputWrapper');
+		const notebook = panel.content;
+		let cells = notebook.model.cells;
+		for (let i = 0; i < cells.length; i++) {
+			const cell = cells.get(i);
+			if(cell.value.text.includes("#@monitor")) {
+				if (!isCodeCellModel(cell)) {
+					throw new Error("cell is not a code cell.");
+				}
+				this.selectedCell = cell;
+				this.selectedCellHTML = cellsFromDomModel.item(i);
+				this.addListener();
+				this.setInfo("Cell found");
+				this.insertAWSMonitor();
+				break;
+			}
+		}
+		if(!this.selectedCell) {
+			this.setInfo("No cell with annotation found");
+		}
 	}
 
     insertAWSMonitor(): void {
-        var cellsFromDomModel = document.getElementsByClassName('lm-Widget p-Widget lm-Panel p-Panel jp-Cell-inputWrapper');
 
-        let cellWithAnnotationFound = false;
-        let cell = null;
-        for(let i = 0; i < cellsFromDomModel.length && cellWithAnnotationFound == false; i++) {
-            cell = cellsFromDomModel.item(i);
-            const commentElementsInsideCell = cell.getElementsByClassName('cm-comment');
-            console.log("Comment cell found");
-
-            for(let j = 0; j < commentElementsInsideCell.length && cellWithAnnotationFound == false; j++) {
-                if(commentElementsInsideCell.item(j).innerHTML == '#@monitor') {
-                    cellWithAnnotationFound = true
-                    console.log("Cell with monitor annotation found");
-                }
-            }
-        }
-
-        if(cellWithAnnotationFound) {
+        if(this.monitorHTLM == null) {
             this.monitorHTLM = document.createElement('div');
             this.monitorHTLM.id = 'monitor';
             this.monitorHTLM.innerHTML = 
@@ -162,7 +148,7 @@ export class ButtonExtension implements DocumentRegistry.IWidgetExtension<Notebo
                                 '<div id="created-bar" style="height: 25px; width: 0px; background-color: #80d2ff">' +
                                 '</div>' +
                             '</div>' +
-                            '<text style="font-weight: 900;">6000 ms</text>' +
+                            '<text id="created-time" style="font-weight: 900;"></text>' +
                         '</div>' +
                         '<div style="height: 5px; width: 500px; margin-top: 10px; background-color: #d6d6d6">' +
                         '</div>' +
@@ -174,12 +160,12 @@ export class ButtonExtension implements DocumentRegistry.IWidgetExtension<Notebo
                                 '<div id="finished-bar" style="height: 25px; width: 0px; background-color: #5bfc60">' +
                                 '</div>' +
                             '</div>' +
-                            '<text style="font-weight: 900;">2000 ms</text>	' +
+                            '<text id="finished-time" style="font-weight: 900;"></text>	' +
                         '</div>' +
                     '</div>' +
                 '</div>'
                 
-            cell.parentNode.insertBefore(this.monitorHTLM, cell.nextElementSibling);
+            this.selectedCellHTML.parentNode.insertBefore(this.monitorHTLM, this.selectedCellHTML.nextElementSibling);
         }
     }
 
@@ -195,49 +181,6 @@ export class ButtonExtension implements DocumentRegistry.IWidgetExtension<Notebo
 		
 		this.lastOutput = "";
 		this.selectedCell.outputs.changed.connect(this.outputSlot, "");
-	}
-
-	async showAWSMonitor(panel: NotebookPanel): Promise<any> {
-
-		const notebook = panel.content;
-		const newCell = notebook.model?.contentFactory.createCodeCell({});
-
-
-		let oldIndex = notebook.activeCellIndex;
-
-		notebook.model.cells.insert(0, newCell);
-		notebook.activeCellIndex = 0;
-
-
-		//notebook.activeCell.hide();
-
-		const cell: ICellModel = notebook.model.cells.get(0);
-		if (!isCodeCellModel(cell)) {
-		throw new Error("cell is not a code cell.");
-		}
-
-		cell.value.text = "from IPython.core.display import display, HTML\ndisplay(HTML('<div style=\"display: flex; justify-content: center; align-items: center\"><div style=\"height: 200px; width: 500px;\"><div style=\"height: 35px; width: 495px; background-color: #ffb029; display: flex; flex-direction: row; justify-content: left; align-items: center; padding-left: 5px\"><text style=\"font-weight: 900; margin-right: 30px\">AWSMonitor</text><text style=\"font-weight: 900\">Partitions: 64</text></div><div style=\"height: 35px; width: 440px; background-color: #d6d6d6; display: flex; flex-direction: row; justify-content: space-between; align-items: center;padding-left: 30px; padding-right: 30px\"><text style=\"font-weight: 900;\">Status</text><text style=\"font-weight: 900\">Progress</text><text style=\"font-weight: 900\">Duration</text></div><div style=\"height: 35px; width: 440px; display: flex; flex-direction: row; justify-content: space-between; align-items: center; margin-top: 10px; padding-left: 30px; padding-right: 30px\"><text style=\"font-weight: 900;\">Created</text><div style=\"height: 25px; width: 250px; border-style: solid; background-color: #d6d6d6\"><div style=\"height: 25px; width: 200px; background-color: #80d2ff\"></div></div><text style=\"font-weight: 900;\">6000 ms</text>	</div><div style=\"height: 5px; width: 500px; margin-top: 10px; background-color: #d6d6d6\"></div><div style=\"height: 35px; width: 440px; display: flex; flex-direction: row; justify-content: space-between; align-items: center; margin-top: 10px; padding-left: 30px; padding-right: 30px\"><text style=\"font-weight: 900;\">Finished</text><div style=\"height: 25px; width: 250px; border-style: solid; background-color: #d6d6d6\"><div style=\"height: 25px; width: 100px; background-color: #5bfc60\"></div></div><text style=\"font-weight: 900;\">2000 ms</text>	</div></div></div>'))";
-
-		await NotebookActions.run(notebook, panel.sessionContext);
-		
-		//console.log(cell.outputs.get(0));
-
-		//notebook.model.cells.remove(0);
-		notebook.activeCellIndex = oldIndex;
-
-		notebook.model.cells.changed.connect(this.allCellsSlot, "");
-
-		const cell2: ICellModel = notebook.model.cells.get(1);
-		if (!isCodeCellModel(cell2)) {
-		throw new Error("cell is not a code cell.");
-		}
-
-		cell2.value.changed.connect(this.inputSlot, "");
-		
-	}
-
-	inputSlot(sender: IObservableString, args: IObservableString.IChangedArgs) {
-		console.log(args);
 	}
 
 	outputSlot = async (sender: IOutputAreaModel, args: IOutputAreaModel.ChangedArgs): Promise<any> => {
@@ -273,23 +216,30 @@ export class ButtonExtension implements DocumentRegistry.IWidgetExtension<Notebo
 
 	setPartitions(partitions: number): void {
 		this.partitions = partitions;
+		this.startTime = Date.now();
 		document.getElementById('partitions-text').textContent = "Number of partitions: " + this.partitions;
 	}
 
 	setCreated(created: number): void {
 		this.created = created + 1;
+		if(this.created === this.partitions) {
+			this.setTimeText(Date.now(), "created-time");
+		}
 		document.getElementById('created-bar').style.width = (250*(this.created)/(this.partitions)).toString() + "px";
 		document.getElementById('created-number').textContent = this.created + "/" + this.partitions;
 	}
 
 	setFinished(finished: number): void {
 		this.finished = finished;
+		if(this.finished === this.partitions) {
+			this.setTimeText(Date.now(), "finished-time");
+		}
 		document.getElementById('finished-bar').style.width = (250*(this.finished)/(this.partitions)).toString() + "px";
 		document.getElementById('finished-number').textContent = this.finished + "/" + this.partitions;
 	}
 
-	allCellsSlot(list: IObservableUndoableList<ICellModel>, args: IObservableList.IChangedArgs<ICellModel>) {
-		console.log(args);
+	setTimeText(time: number, id: string) {
+		document.getElementById(id).textContent = (time - this.startTime) + " ms";
 	}
 
 }
