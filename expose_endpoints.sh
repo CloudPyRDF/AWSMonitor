b349095e2dd647e5821368646da7d3550dcff00f
Kamil Burkiewicz <kamil.burkiewicz@gmail.com>
2021-11-20 23:34:13 +0100

    Update expose_endpoints.sh

--- expose_endpoints.sh
+++ expose_endpoints.sh
@@ -13,14 +13,14 @@ ${marker}
 import requests
 import json
 
-path = '/tmp/token_9238572973'
-
-with open(path, 'r') as f:
-    token_9238572973 = f.read()
+with open('/tmp/token_9238572973', 'r') as f:
+    lines = f.read().splitlines()
+    ___token = lines[0]
+    ___port = lines[1]
 
 AWS.__ProcessAndMerge = AWS.ProcessAndMerge
 def __ProcessAndMerge(self, ranges, mapper, reducer):
-    requests.put('http://localhost:8888/AWSMonitor', params={'token': token_9238572973}, data=json.dumps({'msg': 'START', 'npart': len(ranges)})))
+    requests.put('http://localhost:' + ___port + '/AWSMonitor', params={'token': ___token}, data=json.dumps({'msg': 'START', 'npart': len(ranges)}))
     return self.__ProcessAndMerge(ranges, mapper, reducer)
 AWS.ProcessAndMerge = __ProcessAndMerge
 ${marker}" >> $main_path
@@ -33,11 +33,16 @@ ${marker}
 import requests
 import json
 
+with open('/tmp/token_9238572973', 'r') as f:
+    lines = f.read().splitlines()
+    ___token = lines[0]
+    ___port = lines[1]
+
 AWSServiceWrapper.__invoke_root_lambda = AWSServiceWrapper.invoke_root_lambda
 def __invoke_root_lambda(self, *args, **kwargs):
-    requests.put('http://localhost:8888/AWSMonitor', params={'token': token_9238572973}, data=json.dumps({'msg': 'INV'}))
+    requests.put('http://localhost:' + ___port + '/AWSMonitor', params={'token': ___token}, data=json.dumps({'msg': 'INV'}))
     retval = self.__invoke_root_lambda(*args, **kwargs)
-    requests.put('http://localhost:8888/AWSMonitor', params={'token': token_9238572973}, data=json.dumps({'msg': 'FIN'}))
+    requests.put('http://localhost:' + ___port + '/AWSMonitor', params={'token': ___token}, data=json.dumps({'msg': 'FIN'}))
     return retval
 AWSServiceWrapper.invoke_root_lambda = __invoke_root_lambda
 ${marker}" >> $lambda_path
